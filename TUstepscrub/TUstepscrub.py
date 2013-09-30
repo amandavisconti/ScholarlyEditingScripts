8d4dd945f330c8040a3f1c8e30c8cb45f18d214e
Amanda Visconti <visconti@umd.edu>
2013-09-30 06:42:55 -0400

    Reworded commenting

--- TUstepscrub/TUstepscrub.py
+++ TUstepscrub/TUstepscrub.py
@@ -6,7 +6,7 @@ CREDIT: For the encoding replacement code, this script is based on Eli Courtwrig
 
 LICENSING: Completely free for reuse of any type. Credit to all parties involved appreciated but not required.
 
-PURPOSE: To remove TUstep encoding and line numbering from a directory of .txt files, creating a clean reading text that can also be used for text analysis (e.g. with Juxta).
+PURPOSE: To remove format encoding and line numbering from a directory of .txt files, creating a clean reading text that can also be used for text analysis (e.g. with Juxta).
 
 DIRECTIONS:
 1. These directions assume you have, on your desktop, a folder named TUstepscrub containing both TUstepscrub.py and a folder of your .txt file(s) to be scrubbed named "TUstepfiles". These directions also assume Mac usage (non-Mac users may need to install Python and substitute their command-line program for the Mac terminal).
@@ -17,8 +17,8 @@ and hitting enter.
 	python TUstepscrub.py
 and hit enter.
 
-TUstep ENCODING INCLUDED and CUSTOMIZING THIS SCRIPT:
-This script was developed against the TUstep-encoded Ulysses reading text edited by Hans Walter Gabler; it removes all of the TUstep format encodings I could locate in those files. It may not remove TUstep encoding that does not appear in the Ulysses files, but you can easily customize this script to remove additional TUstep encodings by reading the code comments farther below.
+ENCODING INCLUDED and CUSTOMIZING THIS SCRIPT:
+This script was developed against the encoded Ulysses reading text edited by Hans Walter Gabler; it removes all of the format encodings I could locate in those files. It may not remove encoding that does not appear in the Ulysses files, but you can easily customize this script to remove additional encodings by reading the code comments farther below.
 
 SPECIFIC THINGS REMOVED BY THIS SCRIPT:
 13 format encodings:
@@ -113,6 +113,6 @@ for dname, dirs, files in os.walk("TUstepfiles"):
             f.write(s)
 """
 
-print "Success! The TUstep encodings indicated in the script have been removed from your text files. \nYou'll still need to remove any line-numbering from the files. Currently, I only have a script set up to remove line-numbering and print the results into this window, so you'll need to copy and paste the results you'll see in a moment into a new text file. \nTo strip line numbers, type 'python numberremoval.py TUstepfiles/'filename.txt'' below, replacing 'filename.txt' with a filename from /TUstepscrub/Tustepfiles (keeping the single quotes around the filename so the code can handle the spaces and parantheses in the file name) and then pressing enter. \nYou'll need to do this for each file. \nFor the first file, the command you should enter would be python numberremoval.py TUstepfiles/'U-G (01).txt'"
+print "Success! The format encodings indicated in the script have been removed from your text files. \nYou'll still need to remove any line-numbering from the files. Currently, I only have a script set up to remove line-numbering and print the results into this window, so you'll need to copy and paste the results you'll see in a moment into a new text file. \nTo strip line numbers, type 'python numberremoval.py TUstepfiles/'filename.txt'' below, replacing 'filename.txt' with a filename from /TUstepscrub/Tustepfiles (keeping the single quotes around the filename so the code can handle the spaces and parantheses in the file name) and then pressing enter. \nYou'll need to do this for each file. \nFor the first file, the command you should enter would be python numberremoval.py TUstepfiles/'U-G (01).txt'"
 
 # Line numbering removal: still need to add this. For now, use another utility such as the code the printed instructions above directs you to use (preferred option), or the tool at http://www.gonnalearn.com/code/remove_line_numbers_from_code.htm (this seems to use a PHP script which tries to recognize the format of the line numbering and remove it; I've noticed it is sometimes removing letters/words from the beginning of lines, especially from "Sirens", so you'll want to carefully proofread or locate another tool.)
